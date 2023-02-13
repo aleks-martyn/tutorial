600793bf188787d2ae11638885cbae0a643cbe4a
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-02-13 20:49:43 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-'use srtict'
+"use srtict";
 
 const yearOfBirth = 2006;
-console.log(yearOfBirth); // 2006
+console.log(yearOfBirth);
+
+class User {
+  // Деструктуризуємо об'єкт
+  constructor({ name, email }) {
+    this.name = name;
+    this.email = email;
+  }
+}
+
+const mango = new User({
+  name: "Манго",
+  email: "mango@mail.com",
+});
+console.log(mango);
