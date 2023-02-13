bf35782f2e29c1fc793c006d5a916c50b91ccdd2
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-02-14 00:30:45 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,3 +16,20 @@ const mango = new User({
   email: "mango@mail.com",
 });
 console.log(mango);
+
+console.log("JavaScript is awesome".length);
+console.log("JavaScript is awesome".toUpperCase());
+
+const quantity = 17;
+console.log(typeof quantity);
+
+const username = "Mango";
+console.log("Username is ", username);
+
+const x = 5;
+const y = 10;
+const z = 5;
+
+const valueB = "random string";
+console.log(Number(valueB)); // NaN
+console.log(typeof Number(valueB)); // "number"
