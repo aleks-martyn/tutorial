f4a705324c2099ed81f59fb7930eab5bb21bc6a7
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2026-09-03 23:32:03 +0300

    Update tutorial

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-"use srtict";
-
 const number = 10;
 let string = "";
+
 for (let i = 0; i < number; i += 1) {
   if (i % 2 === 0) {
     continue;
@@ -9,10 +8,13 @@ for (let i = 0; i < number; i += 1) {
   console.log(string += i);
   console.log("Непарне i: ", i); // 1, 3, 5, 7, 9
 }
+
 console.log(string.split('').join(', '));
+
 
 const numbers = [1, 3, 14, 18, 4, 7, 29, 6, 34];
 const threshold = 15;
+
 for (const number of numbers) {
   if (number < threshold) {
     continue;
@@ -24,7 +26,8 @@ for (const number of numbers) {
 
 const a = ['mango'];
 const b = a;
-console.log(a === b);
+console.log(a === b); // true
+
 
 function multiply() {
   let total = 1;
@@ -35,13 +38,16 @@ function multiply() {
 
   return total;
 }
-console.log(multiply(1, 2, 3));
+console.log(multiply(1, 2, 3)); // 6
+
 
 function fn(...args) {
   // Змінна args буде містити повноцінний масив
   console.log(args);
 }
-fn(4, 5, 6);
+
+fn(4, 5, 6); // [4, 5, 6]
+
 
 const book = {
   title: "The Last Kingdom",
@@ -49,6 +55,7 @@ const book = {
   genres: ["historical prose", "adventure"],
   rating: 8.38,
 };
+
 for (const key in book) {
   // Ключ
   console.log(key);
@@ -67,27 +74,33 @@ registerGuest("Манго", function greet(name) {
   console.log(`Ласкаво просимо ${name}.`);
 });
 
+
 const isRecipientAvailable = Math.random() > 0.5;
 console.log(isRecipientAvailable);
+
 
 const numbersus = [5, 10, 15, 20, 25];
 numbersus.forEach((number, index, array) =>
   console.log(`Індекс ${index}, значення ${number}, значення ${array}`)
 );
 
+
 const fruits = [
   { name: "apples", amount: 100 },
   { name: "bananas", amount: 0 },
   { name: "grapes", amount: 50 },
 ];
+
 const anyAvailable = fruits.some(({amount}) => amount > 0);
 console.log(anyAvailable);
+
 
 const total = [2, 7, 3, 14, 6].reduce((previousValue, number) => {
   return previousValue + number;
 });
 
 console.log(total);
+
 
 const tweets = [
   { id: "000", likes: 5, tags: ["js", "nodejs"] },
@@ -105,7 +118,6 @@ const getTags = tweets =>
   }, []);
 
 const tags = getTags(tweets);
-
 console.log(tags);
 
 const getTagStats = (acc, tag) => {
@@ -117,6 +129,7 @@ const getTagStats = (acc, tag) => {
 
   return acc;
 };
+
 const countTags = tags => tags.reduce(getTagStats, {});
 
 const tagCount = countTags(tags);
