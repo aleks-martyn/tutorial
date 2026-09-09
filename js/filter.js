4bd766b02b60c57a872a26463fe96309714ff490
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2026-09-09 23:55:47 +0300

    Create filter.

---
+++ js/filter.js
@@ -0,0 +1,39 @@
+const tech = [
+  { label: 'HTML' },
+  { label: 'CSS' },
+  { label: 'JavaScript' },
+  { label: 'Node.js' },
+  { label: 'React' },
+  { label: 'Vue' },
+  { label: 'Next.js' },
+  { label: 'Mobx' },
+  { label: 'Redux' },
+  { label: 'React Router' },
+  { label: 'GraphQl' },
+  { label: 'PostgreSQL' },
+  { label: 'MongoDB' },
+];
+
+const refs = {
+  list: document.querySelector('.js-list'),
+  input: document.querySelector('#filter'),
+};
+
+refs.input.addEventListener('input', onFilterChange);
+
+const listItemsMarkup = createListItemsMarkup(tech);
+
+refs.list.innerHTML = listItemsMarkup;
+
+function createListItemsMarkup(items) {
+  return items.map(item => `<li>${item.label}</li>`).join('');
+}
+
+function onFilterChange(evt) {
+  const filter = evt.target.value.toLowerCase();
+
+  const filteredItems = tech.filter(t =>
+    t.label.toLowerCase().includes(filter)
+  );
+  console.log(filteredItems);
+}
