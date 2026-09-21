8360bc9bc6426a783287421ee674ef65c28dc5b3
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2026-09-21 23:43:02 +0300

    Create test-2

---
+++ js/test-2.js
@@ -0,0 +1,26 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const listEl = document.querySelector('#ingredients');
+
+const createListItemEl = item => {
+  const listItemEl = document.createElement('li');
+  listItemEl.textContent = item;
+  listItemEl.classList.add('item');
+
+  return listItemEl;
+};
+
+const makeListItems = (items, callback) => items.map(callback);
+
+const listItems = makeListItems(ingredients, createListItemEl);
+
+listEl.append(...listItems);
+
+console.log(listEl);
